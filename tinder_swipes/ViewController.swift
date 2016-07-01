86fbb0bcfdf4caf5fb8a95e96a9803d5ad6ceff2
altonelli <altonelli14@gmail.com>
2016-07-01 15:49:03 -0700

    Initial Commit

---
+++ tinder_swipes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tinder_swipes
+//
+//  Created by Arthur Tonelli on 7/1/16.
+//  Copyright © 2016 Arthur Tonelli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
